eeae6ed8e43e83b821e009306768e454baf060c5
chnoor999 <onlyforpc709@gmail.com>
2024-02-28 18:16:34 +0500

    remake

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-// navigation
-import { NavigationContainer } from "@react-navigation/native";
-// screens
-import StackScreen from "./app/screens/StackScreen";
-import { FavouritesContextProvider } from "./app/store/FavouritesContext";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="light" />
-      <FavouritesContextProvider>
-        <NavigationContainer>
-          <StackScreen />
-        </NavigationContainer>
-      </FavouritesContextProvider>
-    </>
-  );
+    <View>
+      <Text>App</Text>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
